e2f160892cdc222876e9eb829649853cdcf4135f
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-05-16 17:36:25 -0600

    figured out list styled search

--- client/src/pages/navbarPages/searchLoop/search.js
+++ client/src/pages/navbarPages/searchLoop/search.js
@@ -4,11 +4,17 @@ import CardPopup from "../searchLoop/popUpCard.js";
 
 export default function Search() {
   const [cards, setCards] = useState([]);
-  const [buttonPopUp, setButtonPopUp] = useState(false);
+  const [buttonPopUp, setButtonPopUp] = useState(false, "");
+  const [userInput, setUserInput] = useState("");
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
   const searchCard = async (cards) => {
     cards = [];
 
-    let res = await fetch("https://api.scryfall.com/cards/random");
+    let res = await fetch(
+      `https://api.scryfall.com/cards/autocomplete?q=${userInput}`
+    );
     let response = await res.json();
     cards.push(response);
     console.log(cards);
@@ -17,20 +23,34 @@ export default function Search() {
   };
   return (
     <section>
-      <input className="userInput"></input>
-      <button className="userInput" type="button" onClick={searchCard}>
+      <input
+        type="text"
+        id="userInput"
+        placeholder="Card Name"
+        onChange={handleInputChange}
+        value={userInput}
+        className="userInput"
+        onSubmit={searchCard}
+      ></input>
+      <button className="userInput" type="submit" onClick={searchCard}>
         Search
       </button>
-      <ul>
-        {cards.map((cards) => {
+      <ul className="cardListUl">
+        {cards.map((cards, index) => {
           return (
-            <li className="cardList" key={cards.id}>
-              <a onClick={() => setButtonPopUp(true)}>{cards.name}</a>
+            <li className="cardList" key={index}>
+              <a onClick={(e) => setButtonPopUp(true, e.target.value)}>
+                {cards.data}
+              </a>
             </li>
           );
         })}
       </ul>
-      <CardPopup trigger={buttonPopUp} setTrigger={setButtonPopUp}></CardPopup>
+      <CardPopup trigger={buttonPopUp} setTrigger={setButtonPopUp}>
+        {cards.map((cards) => {
+          return <p>{cards.name}</p>;
+        })}
+      </CardPopup>
     </section>
   );
 }
