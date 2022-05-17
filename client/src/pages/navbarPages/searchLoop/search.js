849d55de9c65f4f6a20c524e58844d324c929341
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-05-17 17:31:16 -0600

    I went back to thgis because it made more sense.

--- client/src/pages/navbarPages/searchLoop/search.js
+++ client/src/pages/navbarPages/searchLoop/search.js
@@ -6,79 +6,61 @@ export default function Search() {
   const [cards, setCards] = useState([]);
   const [buttonPopUp, setButtonPopUp] = useState(false, "");
   const [userInput, setUserInput] = useState("");
-  const [userAutofill, setUserAutofill] = useState([]);
+  const [selected, setSelected] = useState("");
 
-  //this is where I am trying to make the second api CALL
-  const handleInputChange = async (userInput) => {
-    setUserInput(userInput);
-    userAutofill = [];
-    let autofill = await fetch(
-      `https://api.scryfall.com/cards/autocomplete?q=${userInput}`
-    );
-    //after api call await the response and push it to userAutofill
-    let autofillRes = await autofill.json();
-    userAutofill.push(autofillRes);
-    // console logs
-    console.log(userAutofill);
-    console.log(userInput);
-    //console.logs
-    //userAutofill is constant error
-    setUserAutofill(userAutofill);
-    return userAutofill;
+  const handleUserChange = (e) => {
+    setUserInput(e.target.value);
   };
+
+  // const userSelect = (event) => {
+  //   console.log(event.target.value);
+  //   setSelected(event.target.value);
+  // };
 
   const searchCard = async (cards) => {
     cards = [];
 
     let res = await fetch(
-      `https://api.scryfall.com/cards/named?exact=${userInput}`
+      `https://api.scryfall.com/cards/autocomplete?q=${userInput}`
     );
     let response = await res.json();
+
     cards.push(response);
-    console.log(cards);
+
     setCards(cards);
     return cards;
   };
   return (
     <section>
-      <input
-        type="text"
-        id="userInput"
-        placeholder="Card Name"
-        onChange={handleInputChange}
-        value={userInput}
-        className="userInput"
-      ></input>
+      <section>
+        <input
+          type="text"
+          id="userInput"
+          placeholder="Card Name"
+          value={userInput}
+          className="userInput"
+          onChange={handleUserChange}
+        ></input>
+      </section>
 
       <button className="userInput" type="submit" onClick={searchCard}>
         Search
       </button>
+
+      {/* trying something start*/}
+
+      {/* trying something end*/}
       <ul className="cardListUl">
-        {cards.map((cards) => {
+        {cards.map((cards, index) => {
           return (
-            <li className="cardList" key={cards.id}>
-              <a onClick={() => setButtonPopUp(true)}>{cards.name}</a>
+            <li className="cardList" key={index}>
+              <a onClick={() => setButtonPopUp(true)}>{cards.data}</a>
             </li>
           );
         })}
       </ul>
-      <CardPopup trigger={buttonPopUp} setTrigger={setButtonPopUp}>
-        {cards.map((cards) => {
-          return (
-            <section className="popUpContainer">
-              <div>
-                <h1 className="popUpText">{cards.name}</h1>
-              </div>
-              <div>
-                <h1 className="popUpText">USD: {cards.prices.usd}$</h1>
-                <h1 className="popUpText">
-                  USD Foil: {cards.prices.usd_foil}$
-                </h1>
-              </div>
-            </section>
-          );
-        })}
-      </CardPopup>
+      {/* here is the popup  */}
+      <CardPopup trigger={buttonPopUp} setTrigger={setButtonPopUp}></CardPopup>
     </section>
   );
 }
