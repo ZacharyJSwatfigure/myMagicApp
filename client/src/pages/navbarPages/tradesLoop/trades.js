1290991e1c1c747bc4caaf9ac3793ffaf1da1977
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-08-20 09:29:37 -0600

    totals the tradeAwayValue now!!

--- client/src/pages/navbarPages/tradesLoop/trades.js
+++ client/src/pages/navbarPages/tradesLoop/trades.js
@@ -293,7 +293,7 @@ export default function Trade() {
                             >
                               {[key]}
                             </a>
-
+                            {/* extra */}
                             <hr />
                           </div>
                         );
